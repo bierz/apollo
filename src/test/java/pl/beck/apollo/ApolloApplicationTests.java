85633ebbe1bdd7fdc0c24f1fa55385f48ba87ec6
klimkiewiczm <miketheklim@icloud.com>
2023-03-20 22:21:28 +0100

    spring boot init

---
+++ src/test/java/pl/beck/apollo/ApolloApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.beck.apollo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApolloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
